218431e49651641807bc0f6c55f4248acb0073ac
Soroushhanifi <ADVANCE@DESKTOP-J0BN8TH>
2025-09-12 13:19:06 +0330

    Implemention Departement Entity

---
+++ Application/CQRS/DepartementCommand.cs
@@ -0,0 +1,89 @@
+﻿using Domain.Entities.Daroo;
+using Infrastructure;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS
+{
+    public record CreateDepartmentCommand(string Name) : IRequest<Guid>;
+
+    public class CreateDepartmentCommandHandler : IRequestHandler<CreateDepartmentCommand, Guid>
+    {
+        private readonly DarooDbContext _context;
+
+        public CreateDepartmentCommandHandler(DarooDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Guid> Handle(CreateDepartmentCommand request, CancellationToken cancellationToken)
+        {
+            var department = new Department
+            {
+                Name = request.Name,
+                CreateDate = DateTime.UtcNow
+            };
+
+            _context.Departments.Add(department);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return department.Id;
+        }
+    }
+
+    // Update Command
+    public record UpdateDepartmentCommand(int Id, string Name) : IRequest<bool>;
+
+    public class UpdateDepartmentCommandHandler : IRequestHandler<UpdateDepartmentCommand, bool>
+    {
+        private readonly DarooDbContext _context;
+
+        public UpdateDepartmentCommandHandler(DarooDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(UpdateDepartmentCommand request, CancellationToken cancellationToken)
+        {
+            var department = await _context.Departments.FindAsync(request.Id, cancellationToken);
+
+            if (department == null)
+                return false;
+
+            department.Name = request.Name;
+            department.ModifyDate = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+
+    // Delete Command
+    public record DeleteDepartmentCommand(int Id) : IRequest<bool>;
+
+    public class DeleteDepartmentCommandHandler : IRequestHandler<DeleteDepartmentCommand, bool>
+    {
+        private readonly DarooDbContext _context;
+
+        public DeleteDepartmentCommandHandler(DarooDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(DeleteDepartmentCommand request, CancellationToken cancellationToken)
+        {
+            var department = await _context.Departments.FindAsync(request.Id, cancellationToken);
+
+            if (department == null)
+                return false;
+
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
--- Infrastructure/DarooDbContext.cs
+++ Infrastructure/DarooDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities.Daroo;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,8 @@ namespace Infrastructure
     public class DarooDbContext : DbContext
     {
         public DarooDbContext(DbContextOptions<DarooDbContext> options) : base(options) { }
+
+        public DbSet<Department> Departments { get; set; }
 
     }
 }
